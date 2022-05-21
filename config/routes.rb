4188ba51d8f6231d37b66f0385c730abcbb50121
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-05-21 12:55:17 -0300

    add devise add tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # devise_scope :user do
+  #
+  #   get '/users/sign_out', to: 'users/sessions#destroy'
+  # end
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#home"
 end
