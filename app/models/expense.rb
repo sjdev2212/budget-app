ee9d305a49988f224148a8643701596643971718
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-05-25 15:08:15 -0300

    added functionality to categories

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,8 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  validates :name, presence: true
+  validates :name, presence: { message: 'This field can not be empty' }
+  validates :amount,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'The input should be greater or equal to 0' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   has_many :categories, foreign_key: 'user_id', class_name: 'Category', dependent: :delete_all
   has_many :expenses, foreign_key: 'user_id', class_name: 'Expense', dependent: :delete_all
+
+
+  def all_expenses
+    categories.each { |cat| total += cat.total_expenses }
+    total
+  end
+
+
 end
