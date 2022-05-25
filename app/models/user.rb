aa08043a2779f9c3b1181502a5927ee0a866c8a5
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-05-25 15:12:45 -0300

    fix bug and linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,9 @@ class User < ApplicationRecord
   has_many :categories, foreign_key: 'user_id', class_name: 'Category', dependent: :delete_all
   has_many :expenses, foreign_key: 'user_id', class_name: 'Expense', dependent: :delete_all
 
-
   def all_expenses
+    total = 0
     categories.each { |cat| total += cat.total_expenses }
     total
   end
-
-
 end
